5e5bb06b7e95002c7381badcc7b4dc45424d742b
Rory MacDonald <rorymacdonald6@hotmail.com>
2017-01-22 21:26:28 +0000

    DeckTest HandTest done

--- MyApplication/app/src/test/java/example/codeclan/com/cardproject/DeckTest.java
+++ MyApplication/app/src/test/java/example/codeclan/com/cardproject/DeckTest.java
@@ -1,10 +1,32 @@
 package example.codeclan.com.cardproject;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by user on 22/01/2017.
  */
 public class DeckTest {
+
+    Deck setOfCards;
+    Player player;
+
+    @Before
+    public void beforeEach(){
+        setOfCards = new Deck(new ArrayList<Card>());
+        player = new Player("Rory", new Hand());
+    }
+
+//    will draw card at index 0, right now is 2diamonds
+
+    @Test
+    public void canDealCard() {
+        setOfCards.drawCard(player);
+        assertEquals(2, player.getBlackjackHandValue());
+    }
 
 }
--- MyApplication/app/src/test/java/example/codeclan/com/cardproject/HandTest.java
+++ MyApplication/app/src/test/java/example/codeclan/com/cardproject/HandTest.java
@@ -1,10 +1,31 @@
 package example.codeclan.com.cardproject;
 
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by user on 22/01/2017.
  */
 public class HandTest {
+    Hand hand;
+    Card card1;
+    Card card2;
+
+
+    @Before
+    public void before() {
+        hand = new Hand();
+        card1 = new Card(Rank.DEUCE, Suit.DIAMONDS);
+        card2 = new Card(Rank.THREE, Suit.CLUBS);
+        hand.addCardToHand(card1);
+        hand.addCardToHand(card2);
+    }
+
+    @Test
+    public void canGetBlackjackValueOfHand(){
+        assertEquals(5, hand.getBlackjackValue());
+    }
 
 }
