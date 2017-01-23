d8057ae3c71a9a4c77368068447f54e157d7d377
Rory MacDonald <rorymacdonald6@hotmail.com>
2017-01-23 01:15:27 +0000

    Basic Activity made, not functional

--- MyApplication/app/src/main/java/example/codeclan/com/cardproject/RulesActivity.java
+++ MyApplication/app/src/main/java/example/codeclan/com/cardproject/RulesActivity.java
@@ -2,6 +2,8 @@ package example.codeclan.com.cardproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class RulesActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class RulesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(getClass().toString(), "onCreate got called");
+    }
+
+
+    public void onPlayButtonClick(View view) {
     }
 }
